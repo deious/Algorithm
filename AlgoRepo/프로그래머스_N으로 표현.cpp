069478955af21a1b2a890d965948a60b1eeda4cc
deious <gkstjddlekd@gmail.com>
2021-01-02 23:56:12 +0900

    2021-01-02 프로그래머스

---
+++ AlgoRepo/프로그래머스_N으로 표현.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int ansNum, nCount, answer = 9;
+
+void calculate(int num, int count)
+{
+	if (count > 8) return;
+
+	if (num == ansNum)
+	{
+		answer = min(answer, count);
+		return;
+	}
+
+	int tempNum = 0;
+
+	for (int i = 1; i <= 9; i++)
+	{
+		tempNum = tempNum * 10 + nCount;
+		calculate(num + tempNum, count + i);
+		calculate(num - tempNum, count + i);
+		if (num != 0)
+		{
+			calculate(num * tempNum, count + i);
+			calculate(num / tempNum, count + i);
+		}
+	}
+}
+
+int solution(int N, int number) {
+	ansNum = number, nCount = N;
+	calculate(0, 0);
+
+	if (answer == 9) return -1;
+	else return answer;
+}

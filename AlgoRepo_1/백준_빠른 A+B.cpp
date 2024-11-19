b2525b2d4777239ffc1345ccf41c7f74aa0d995b
HanSung <gkstjddlekd@gmail.com>
2024-11-19 23:31:08 +0900

    2024-11-19 백준

---
+++ AlgoRepo_1/백준_빠른 A+B.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int T, A, B;
+	cin >> T;
+
+	while (T--)
+	{
+		cin >> A >> B;
+		cout << A + B << "\n";
+	}
+
+	return 0;
+}
